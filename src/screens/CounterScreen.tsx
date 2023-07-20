d41499ddd53a56f6fb2e8bf61af853fdca0fcbc6
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-07-20 19:52:25 +0200

    Stylesheet

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,29 +1,34 @@
 import {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState<number>(0);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+    <View style={styles.container}>
       <Text
-        style={{
-          fontSize: 50,
-        }}>
+        style={styles.counterLabel}>
         Counter: {counter}
       </Text>
       <TouchableOpacity onPress={() => setCounter(prev => prev + 1)}>
-        <Text style={{
-            backgroundColor: "red",
-            fontSize: 50,
-            borderRadius: 15
-        }}>+1</Text>
+        <Text style={styles.button}>+1</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  counterLabel: {
+    fontSize: 50
+  },
+  button: {
+    backgroundColor: 'red',
+    fontSize: 50,
+    borderRadius: 15,
+  },
+});
