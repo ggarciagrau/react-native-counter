a2e5340bd9cadbd6046069cfc42465a2b0ae552a
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2023-07-22 19:25:43 +0200

    BoxObjectModel - SafeAreaView component anti-notch

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { HelloWorldScreen } from "./src/screens/HelloWorldScreen";
-import { CounterScreen } from "./src/screens/CounterScreen";
+import {SafeAreaView} from 'react-native';
+import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
+import {CounterScreen} from './src/screens/CounterScreen';
+import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 
 export const App = () => {
   return (
-    <CounterScreen />
+    <SafeAreaView>
+      <BoxObjectModelScreen />
+    </SafeAreaView>
   );
 };
 
